8657914f3bd5b18cc21214549800f30a89a0ca55
José Quinde <49071271+JoseQ1996@users.noreply.github.com>
2023-01-03 14:08:37 -0500

    carrito borrar productos

--- src/app/web-app/pages/carrito/carrito.page.ts
+++ src/app/web-app/pages/carrito/carrito.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductoService } from 'src/app/api/producto.service';
 import { Detalle } from 'src/app/entidades';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrito',
@@ -8,12 +9,45 @@ import { Detalle } from 'src/app/entidades';
   styleUrls: ['./carrito.page.scss'],
 })
 export class CarritoPage implements OnInit {
-  detalles:Detalle[]=[]
-  constructor(private productoService:ProductoService) { }
+  detalles: Detalle[] = []
+  constructor(private productoService: ProductoService) { }
 
   ngOnInit() {
-    this.detalles=this.productoService.listarCarrito();
+    this.detalles = this.productoService.listarCarrito();
+
+  }
+  eliminar(id: number) {
+
+
+    console.log("entrando")
+    Swal.fire({
+      title: '¿Esta seguro?',
+      heightAuto: false,
+      text: `Estas a punto de eliminar el Producto del Carrito`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const found = this.detalles.findIndex((d: any) => d.servicioId === id)
+        this.detalles.splice(found, 1)
+        console.log(found, this.detalles)
+        localStorage.removeItem('detalles')
+        localStorage.setItem('detalles', JSON.stringify(this.detalles))
+        location.href = '/web/carrito'
+        Swal.fire(
+          '¡Eliminado!',
+          `Producto eliminado`,
+          'success'
+        )
+      }
+      
+    });
+  }
 
   }
 
-}
+
--- src/app/web-app/pages/generar-factura/generar-factura.page.ts
+++ src/app/web-app/pages/generar-factura/generar-factura.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ClienteService } from 'src/app/api/cliente.service';
+import { ProductoService } from 'src/app/api/producto.service';
+import { Cliente, Detalle } from 'src/app/entidades';
 
 @Component({
   selector: 'app-generar-factura',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generar-factura.page.scss'],
 })
 export class GenerarFacturaPage implements OnInit {
+  registerForm: FormGroup = this.fb.group({
+    identificacionNumero: ['', [Validators.required]],
 
-  constructor() { }
+  })
+
+  cliente: Cliente | undefined;
+  detalles: Detalle[] = []
+  total: number = 0
+  subtotal: number = 0
+  iva: number = 0
+  fecha: number = Date.now()
+  constructor(private fb: FormBuilder,
+    private clienteService: ClienteService,
+    private productoService: ProductoService) { }
 
   ngOnInit() {
+    this.detalles = this.productoService.listarCarrito();
+    this.registerForm.valueChanges
+      .subscribe(res => {
+        const cedula: string = res['identificacionNumero'];
+        if (cedula.length > 9) {
+          this.clienteService.getClienteByCedula(cedula)
+            .subscribe(cliente => {
+              this.cliente = cliente;
+            })
+        } else {
+          this.cliente = undefined;
+        }
+      })
+      for(let d of this.detalles){
+        console.log(d)
+        this.subtotal+=d.total
+      }
+      this.iva=this.subtotal*0.12
+      this.total=this.subtotal+this.iva
+  }
+  facturar() {
+
   }
 
 }
